9bcca643b3dbae12cd5b70a8e9c0fe837b75eb38
thedavidgilmore00@gmail.com <thedavidgilmore00@gmail.com>
2018-08-11 01:20:06 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+
+function addvar(a, b) {
+  return a + b
+}
+
+function subtract5() {
+  a - b
+}
+
+function multiply() {
+  a * b
+}
+
+function divide() {
+  a / b
+}
+
+function inc() {
+  NumA++
+}
+
+function dec() {
+  NumA--
+}
