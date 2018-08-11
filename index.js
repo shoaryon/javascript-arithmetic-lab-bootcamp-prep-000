aafc7b846dc2601efd194034a6911bcdc399d3d0
thedavidgilmore00@gmail.com <thedavidgilmore00@gmail.com>
2018-08-11 01:22:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ function add(a, b) {
   return a + b
 }
 
-function subtract5() {
-  a - b
+function subtract(a, b) {
+  return a - b
 }
 
-function multiply() {
-  a * b
+function multiply(a, b) {
+  return a * b
 }
 
-function divide() {
-  a / b
+function divide(a, b) {
+  return a / b
 }
 
 function inc() {
-  NumA++
+  return NumA++
 }
 
 function dec() {
